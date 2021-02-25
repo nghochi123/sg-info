b54f725b74a860f7b53b897002d8db6f1d9c9f6c
nghochi123 <nghochi123@gmail.com>
2021-02-25 23:19:13 +0800

    test

--- src/scripts/getFullBusInfo.js
+++ src/scripts/getFullBusInfo.js
@@ -3,10 +3,11 @@ const axios = require('axios');
 // const base = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?"
 // const headers = { 'AccountKey': 'ZLvdyLjWTPCnY52q09NC1g==', 'accept': 'application/json'}
 
+const link = "https://express-server-test-hchc.herokuapp.com"
 
 const fullBus = async () => {
     try {
-        const res = await axios.get(`http://localhost:9000/bus/allbuses`);
+        const res = await axios.get(`${link}/bus/allbuses`);
         let info = res.data;
         if(!info) return false;
         return info;
--- src/scripts/getbusinfo.js
+++ src/scripts/getbusinfo.js
@@ -3,10 +3,11 @@ const axios = require('axios');
 // const base = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?"
 // const headers = { 'AccountKey': 'ZLvdyLjWTPCnY52q09NC1g==', 'accept': 'application/json'}
 
+const link = "https://express-server-test-hchc.herokuapp.com"
 
 const getBus = async (busCode) => {
     try {
-        const res = await axios.get(`http://localhost:9000/bus/${busCode}`);
+        const res = await axios.get(`${link}/bus/${busCode}`);
         let info = res.data.Services;
         if(!info) return false;
         return info;
